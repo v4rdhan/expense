e4586a52460732dfd009b403ccf98ca385dbe703
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2023-12-18 23:47:59 +0530

    reverted the changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Header from "./component/Header";
+import Header from "./component/Header/Header";
+import Expectation from "./component/Exp-Rel/Expectation";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Expectation></Expectation>
     </div>
   );
 }
