73e1d63f94db9b01e2c8d8802d0358e4211b034e
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2023-12-20 21:30:37 +0530

    Expectation Value (#2)
    used useState to set and use the expectationValue in chart or graph

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Header from "./component/Header/Header";
 import Expectation from "./component/Exp-Rel/Expectation";
 import Chart from "./component/Chart/Chart.js";
+import { useState } from "react";
 
 function App() {
+  const [expectationValue, setExpectationValue] = useState(0);
   return (
     <div className="App">
       <Header></Header>
-      <Expectation></Expectation>
-      <Chart></Chart>
+      <Expectation
+        expectationValue={expectationValue}
+        setExpectationValue={setExpectationValue}
+      ></Expectation>
+      <Chart
+        expectationValue={expectationValue}
+        setExpectationValue={setExpectationValue}
+      ></Chart>
     </div>
   );
 }
