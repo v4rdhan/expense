2bef6cef748dbfead9fd760619d7335fc5ff1a3c
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2023-12-20 21:29:12 +0530

    Expectation Value
    used useState to set and use the expectationValue in chart or graph

--- src/component/Exp-Rel/Expectation.js
+++ src/component/Exp-Rel/Expectation.js
@@ -2,10 +2,16 @@ import "./Expectation.css";
 
 import TextField from "@mui/material/TextField";
 
-export default function Expectation() {
+export default function Expectation({ expectationValue, setExpectationValue }) {
   return (
     <div className="input-area">
-      <TextField id="outlined-basic" label="Expectation" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="Expectation"
+        variant="outlined"
+        onChange={(event) => setExpectationValue(event.target.value)}
+      />
+      console.log(expectationValue);
       <TextField id="outlined-basic" label="Reality" variant="outlined" />
     </div>
   );
