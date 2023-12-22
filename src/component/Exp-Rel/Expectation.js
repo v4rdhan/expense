16594eeedec16e621524ddee58f91275d704f3fb
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2023-12-22 21:04:11 +0530

    Added Popup
    Added popup in the Drawer

--- src/component/Exp-Rel/Expectation.js
+++ src/component/Exp-Rel/Expectation.js
@@ -11,7 +11,7 @@ export default function Expectation({ expectationValue, setExpectationValue }) {
         variant="outlined"
         onChange={(event) => setExpectationValue(event.target.value)}
       />
-      console.log(expectationValue);
+
       <TextField id="outlined-basic" label="Reality" variant="outlined" />
     </div>
   );
