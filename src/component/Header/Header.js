e6617a670a9c81b335e2b178676bd037c841ce3d
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2023-12-25 14:09:10 +0530

    expense value functionality (#7)
    Added the functionality for  expense value/reality value.
    added json for months, instead of array.

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,10 +2,14 @@ import Avatar from "@mui/material/Avatar";
 import AppDrawer from "../Drawer/Drawer";
 import "./Header.css";
 
-export default function Header() {
+export default function Header({ expenseValues, setExpenseValues, Labels }) {
   return (
     <div className="header">
-      <AppDrawer></AppDrawer>
+      <AppDrawer
+        Labels={Labels}
+        expenseValues={expenseValues}
+        setExpenseValues={setExpenseValues}
+      ></AppDrawer>
       <Avatar>V</Avatar>
     </div>
   );
